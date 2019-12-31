48c9c559a70ad2f509ed737174d4fcdb6887f029
Galang Setianto <galangotnaites@gmail.com>
2019-12-31 10:36:44 +0700

    galang

--- application/controllers/admin/Komentar.php
+++ application/controllers/admin/Komentar.php
@@ -35,31 +35,31 @@ class Komentar extends CI_Controller {
 	}
 
 	function balas(){
-		$email = $this->input->post('email');
-		$balasan = $this->input->post('email');
 		$ci = get_instance();
 	    $ci->load->library('email');
 	    $config['protocol'] = "smtp";
 	    $config['smtp_host'] = "ssl://smtp.gmail.com";
 	    $config['smtp_port'] = "465";
-	    $config['smtp_user'] = "einfostmikwp@gmail.com";
-	    $config['smtp_pass'] = "gottic76";
+	    $config['smtp_user'] = "bappedapkl.kab@gmail.com";
+	    $config['smtp_pass'] = "bappeda_123";
 	    $config['charset'] = "utf-8";
 	    $config['mailtype'] = "html";
 	    $config['newline'] = "\r\n";
 	    $ci->email->initialize($config);
-	    $ci->email->from('einfostmikwp@gmail.com', 'BAPPEDA LITBANG');
-	    $ci->email->to('galangotnaites@gmail.com');
-	    $ci->email->subject('Balasan Admin Bappeda Litbang');
-	    $ci->email->message($balasan." <br/> <br/> Hormat Kami, <br/><br/><br/>Admin Bappeda Litbang Kab. Pekalongan");
-
+	    $ci->email->from('bappedapkl.kab@gmail.com', 'BAppeda Litbang Kabupaten Pekalongan');
+	    $list = array($_POST['email']);
+	    $ci->email->to($list);
+	    $ci->email->subject('Kontak Bappeda Litbang Kabupaten Pekalongan');
+	    $ci->email->message($_POST['balasan']);
 	    if ($this->email->send()) {
-	        $this->session->set_flashdata('alert','<div class="alert alert-info alert-dismissible fade show" role="alert"> <button type="button" class="close" data-dismiss="alert" aria-label="Close"> <span aria-hidden="true"><i class="far fa-trash"></i></span> </button> <strong>Berhasil!</strong> Balasan Anda Terkirim.</div>');
+	    	$this->session->set_flashdata('alert','<div class="alert alert-info alert-dismissible fade show" role="alert"> <button type="button" class="close" data-dismiss="alert" aria-label="Close"> <span aria-hidden="true"><i class="far fa-trash"></i></span> </button> <strong>Berhasil!</strong> Balasan Anda Terkirim.</div>');
 	    } else {
-	        show_error($this->email->print_debugger());
+	        // show_error($this->email->print_debugger());
+	        $this->session->set_flashdata('alert','<div class="alert alert-danger alert-dismissible fade show" role="alert"> <button type="button" class="close" data-dismiss="alert" aria-label="Close"> <span aria-hidden="true"><i class="far fa-trash"></i></span> </button> <strong>Berhasil!</strong> Balasan Gagal Terkirim.</div>');
+	        redirect('index.php/admin/pesan');
 	    }
-	    
-		$data = array(
+
+	    $data = array(
         		'balasan' 		=> $this->input->post('balasan'),
         		'id_komentar' 	=> $this->input->post('id_komentar'),
         		'tgl_balas' 	=> date('Y-m-d H:i:s'),
@@ -71,13 +71,41 @@ class Komentar extends CI_Controller {
 	    $status = array( 
 		    'status'      => 2
 		);
+		$this->db->where('id',$this->input->post('id_komentar'))->update('komentar',$status);
 
 		$notif = $this->db->where('id',$data['id_komentar'])->get('komentar')->row_array();
 
 		one_signal_no_image($notif['user_notif'],'Komentar anda Sudah di Tanggapi',$data['balasan'],base_url('berita/detail/').$notif['link']);
-
-		$this->db->where('id',$this->input->post('id_komentar'))->update('komentar',$status);
+		
 		redirect('admin/komentar');
+
+
+
+		// $email = $this->input->post('email');
+		// $balasan = $this->input->post('balasan');
+		// $ci = get_instance();
+	 //    $ci->load->library('email');
+	 //    $config['protocol'] = "smtp";
+	 //    $config['smtp_host'] = "ssl://smtp.gmail.com";
+	 //    $config['smtp_port'] = "465";
+	 //    $config['smtp_user'] = "bappedapkl.kab@gmail.com";
+	 //    $config['smtp_pass'] = "bappeda_123";
+	 //    $config['charset'] = "utf-8";
+	 //    $config['mailtype'] = "html";
+	 //    $config['newline'] = "\r\n";
+	 //    $ci->email->initialize($config);
+	 //    $ci->email->from('bappedapkl.kab@gmail.com', 'BAPPEDA LITBANG');
+	 //    $ci->email->to('email');
+	 //    $ci->email->subject('Balasan Admin Bappeda Litbang');
+	 //    $ci->email->message($balasan." <br/> <br/> Hormat Kami, <br/><br/><br/>Admin Bappeda Litbang Kab. Pekalongan");
+
+	 //    if ($this->email->send()) {
+	 //        $this->session->set_flashdata('alert','<div class="alert alert-info alert-dismissible fade show" role="alert"> <button type="button" class="close" data-dismiss="alert" aria-label="Close"> <span aria-hidden="true"><i class="far fa-trash"></i></span> </button> <strong>Berhasil!</strong> Balasan Anda Terkirim.</div>');
+	 //    } else {
+	 //        show_error($this->email->print_debugger());
+	 //    }
+	    
+	
 	}
 }
 
